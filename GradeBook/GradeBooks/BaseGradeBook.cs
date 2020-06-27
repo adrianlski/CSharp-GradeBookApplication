4c295cdb65fcd93e6fda8de9a99b7c0fa7632f84
Adrian <adrianlski@gmail.com>
2020-06-27 15:19:27 +0200

    Progress

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -111,7 +111,7 @@ namespace GradeBook.GradeBooks
         {
             var extraPoint = 0;
 
-            if (IsWeighted && studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
             {
                 extraPoint = 1;
             }
